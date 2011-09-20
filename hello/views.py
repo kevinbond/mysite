6c4492bc95403fa9aca1fe0d6d02d95030ae5fd2
Kevin Bond <kbond@voxeo.com>
2011-09-20 14:11:04 -0400

    Adjusted README

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,21 @@
 # Create your views here.
-from django.http import HttpResponse
-from tropo import Tropo, Session
+#sends information back to Tropo
+from django.http import HttpResponse  
+#This will allow you to call Tropo functions
+from tropo import Tropo 
+#This will bypass authorization
 from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 
  
-def hello(request):
+#hello was are method name called by urls.py 
+def hello(request): 
   t = Tropo()
-  msg = request.POST['msg']
-  json = t.say("you just said: " + msg)
-  json = t.RenderJson(json)
-  return HttpResponse(json)
+  #This is the initial text that you want
+  msg = request.POST['msg'] 
+  #This sends a text reply back
+  json = t.say("you just said: " + msg) 
+  #This renders the reply into JSON so Tropo can read it
+  json = t.RenderJson(json) 
+  #This sends the JSON to Tropo
+  return HttpResponse(json) 
